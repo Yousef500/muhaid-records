a175ef8086a05a43c61d39e7e52f8c10b9756420
Yousef500 <Kazuya14725@gmail.com>
2022-07-06 06:39:31 +0300

    Error with images

--- pages/api/save-project.js
+++ pages/api/save-project.js
@@ -1,6 +1,6 @@
 import fs from "fs";
-import {connectToDatabase} from "../../lib/mongodb";
 import jwt from "jsonwebtoken";
+import {connectToDatabase} from "../../lib/mongodb";
 
 const handler = async (req, res) => {
     const {newProject} = req.body;
@@ -8,46 +8,69 @@ const handler = async (req, res) => {
     const date = Date.now();
 
     if (token) {
-        const publicKey = await fs.promises.readFile('./src/app/public.pem');
-        const verifiedUser = await jwt.verify(token, publicKey, {algorithm: 'RS256'});
+        const publicKey = await fs.promises.readFile("./src/app/public.pem");
+        const verifiedUser = await jwt.verify(token, publicKey, {
+            algorithm: "RS256",
+        });
         if (verifiedUser?.email && newProject?.projectName) {
             const {projectName} = newProject;
             try {
                 const {db} = await connectToDatabase();
-                const project = await db.collection('Projects').findOne({projectName});
-                if (project?._id) return res.status(400).json({message: 'المشروع موجود بالفعل'});
-                const counter = await db.collection('Counters').findOneAndUpdate({_id: 'projectId'}, {$inc: {sequence_value: 1}});
+                const project = await db.collection("Projects").findOne({projectName});
+                if (project?._id) return res.status(400).json({message: "المشروع موجود بالفعل"});
+                const counter = await db
+                    .collection("Counters")
+                    .findOneAndUpdate({_id: "projectId"}, {$inc: {sequence_value: 1}});
                 const sequenceValue = counter.value.sequence_value;
-                const result = await db.collection('Projects').insertOne({
+                const basePath = `./static/images/${sequenceValue}`;
+                await fs.promises.mkdir(`${basePath}`, {recursive: true});
+
+                let dbImages = [];
+                await newProject.images.map(async (imageObject) => {
+                    const base64Image = imageObject.src;
+                    const imageBuffer = await Buffer.from(base64Image.split("base64,")[1], "base64");
+                    await fs.promises.writeFile(`${basePath}/${imageObject.name}`, imageBuffer);
+
+                    dbImages.push({
+                        src: `${basePath}/${imageObject.name}`,
+                        name: imageObject.name,
+                        type: imageObject.type,
+                    });
+                });
+
+                console.log('dbImages', dbImages)
+
+                const result = await db.collection("Projects").insertOne({
                     _id: sequenceValue,
                     ...newProject,
+                    images: dbImages,
                     createdAt: date,
                     updatedAt: date,
-                    mainImage: newProject.images[0],
+                    mainImage: dbImages[0],
                     createdBy: verifiedUser.email,
-                    updatedBy: verifiedUser.email
+                    updatedBy: verifiedUser.email,
                 });
                 if (result.acknowledged) {
                     return res.status(201).json();
                 } else {
-                    return res.status(400).json({message: 'لقد حدث خطأ ما'})
+                    return res.status(400).json({message: "لقد حدث خطأ ما"});
                 }
             } catch (e) {
-                console.log({e})
-                return res.status(400).json({message: 'لقد حدث خطأ ما'});
+                console.log({e});
+                return res.status(400).json({message: "لقد حدث خطأ ما"});
             }
         }
     } else {
-        return res.status(403).json({message: 'الرجاء تسجيل الدخول أولا'});
+        return res.status(403).json({message: "الرجاء تسجيل الدخول أولا"});
     }
-}
+};
 
 export default handler;
 
 export const config = {
     api: {
         bodyParser: {
-            sizeLimit: '5mb'
-        }
-    }
-}
+            sizeLimit: "5mb",
+        },
+    },
+};
--- pages/index.js
+++ pages/index.js
@@ -1,96 +1,137 @@
-import {connectToDatabase} from "../lib/mongodb";
-import {useDispatch} from "react-redux";
-import {useEffect} from "react";
-import {setProjects} from "../src/app/slices/projectsSlice";
-import Navbar from "../components/Navbar";
+import { AddCircleOutline } from "@mui/icons-material";
+import { Button, Container, Grid } from "@mui/material";
+import fs from "fs";
 import Head from "next/head";
-import {Button, Container, Grid} from "@mui/material";
-import SearchComponent from "../components/Search";
 import Link from "next/link";
-import {AddCircleOutline} from "@mui/icons-material";
-import Projects from "../components/Projects";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import Navbar from "../components/Navbar";
 import CustomPagination from "../components/Pagination";
+import Projects from "../components/Projects";
+import SearchComponent from "../components/Search";
+import { connectToDatabase } from "../lib/mongodb";
+import { setProjects } from "../src/app/slices/projectsSlice";
 
 export async function getServerSideProps(ctx) {
     const accessToken = ctx.req.cookies.access_token;
     if (!accessToken) {
         return {
             redirect: {
-                destination: '/sign-in',
-                permanent: false
-            }
-        }
+                destination: "/sign-in",
+                permanent: false,
+            },
+        };
     }
 
-    const {currentPage, pageSize} = ctx.query;
+    try {
+        const { currentPage, pageSize } = ctx.query;
 
-    const pageNumber = Number(currentPage)
-    const perPage = Number(pageSize)
-    const skipParams = await (pageNumber - 1) * perPage;
-    const projection = {projectName: 1, projectAddress: 1, "mainImage.src": 1}
-    const {db} = await connectToDatabase();
-    const count = await db.collection('Projects').estimatedDocumentCount();
-    const cursor = await db.collection('Projects').find({}, {
-        // sort: {createdAt: -1},
-        skip: skipParams,
-        limit: perPage,
-        "hint": "home_page"
-    }).project(projection);
-    const projects = await cursor.toArray()
-    return {
-        props: {
-            projects,
-            count,
-            pageNumber,
-            perPage
-        }
+        const pageNumber = Number(currentPage);
+        const perPage = Number(pageSize);
+        const skipParams = (pageNumber - 1) * perPage;
+        const projection = {
+            projectName: 1,
+            projectAddress: 1,
+            mainImage: 1,
+        };
+        const { db } = await connectToDatabase();
+        const count = await db.collection("Projects").estimatedDocumentCount();
+        const cursor = await db
+            .collection("Projects")
+            .find(
+                {},
+                {
+                    // sort: {createdAt: -1},
+                    skip: skipParams,
+                    limit: perPage,
+                    // hint: "home_page",
+                }
+            )
+            .project(projection);
+        const initialProjects = await cursor.toArray();
+        console.log("initialProjects", initialProjects);
+        const projects = await initialProjects.map(async (proj) => {
+            const image = await fs.promises.readFile(proj.mainImage.src, "base64");
+            return {
+                ...proj,
+                mainImage: {
+                    ...proj.mainImage,
+                    src: `data:${proj.mainImage.type};base64,${image}`,
+                },
+            };
+        });
+
+        console.log("projects", projects);
+        return {
+            props: {
+                projects,
+                count,
+                pageNumber,
+                perPage,
+            },
+        };
+    } catch (e) {
+        console.error({ e });
+        return {
+            props: {
+                projects: [],
+            },
+        };
     }
 }
 
-
-export default function Home({projects, count, pageNumber, perPage}) {
+export default function Home({ projects, count, pageNumber, perPage }) {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(setProjects({projects, count, pageNumber, perPage}))
-    })
+        dispatch(setProjects({ projects, count, pageNumber, perPage }));
+    });
 
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <Head>
                 <title>سجلَات المُحايد</title>
-                <meta name="description" content="Al Muhaid Redocrds"/>
-                <link rel="icon" href="/mceicon.png"/>
+                <meta name="description" content="Al Muhaid Redocrds" />
+                <link rel="icon" href="/mceicon.png" />
             </Head>
 
-            <Container maxWidth={'fluid'}>
-
-                <Grid container
-                      alignItems="center"
-                      justifyContent="center" spacing={{xs: 1, sm: 2}} m={'auto'}>
+            <Container maxWidth={"fluid"}>
+                <Grid
+                    container
+                    alignItems="center"
+                    justifyContent="center"
+                    spacing={{ xs: 1, sm: 2 }}
+                    m={"auto"}
+                >
                     <Grid item xs={12}>
-                        <SearchComponent/>
+                        <SearchComponent />
                     </Grid>
 
-                    <Grid item xs={4}/>
+                    <Grid item xs={4} />
                     <Grid item xs={4}>
-                        <Link href={'/add-project'}>
-                            <Button fullWidth variant={'contained'} size={'large'}
-                                    startIcon={<AddCircleOutline/>}>إضافة مشروع</Button>
+                        <Link href={"/add-project"}>
+                            <Button
+                                fullWidth
+                                variant={"contained"}
+                                size={"large"}
+                                startIcon={<AddCircleOutline />}
+                            >
+                                إضافة مشروع
+                            </Button>
                         </Link>
                     </Grid>
-                    <Grid item xs={4}/>
+                    <Grid item xs={4} />
 
                     <Grid item xs={12}>
-                        <Projects/>
+                        <Projects />
                     </Grid>
 
                     <Grid item xs={12} my={4}>
-                        <CustomPagination/>
+                        <CustomPagination />
                     </Grid>
                 </Grid>
             </Container>
         </>
-    )
+    );
 }
