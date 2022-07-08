4b2acef98c661fc82543965037ba776ee1d7e998
Yousef500 <42538163+Yousef500@users.noreply.github.com>
2022-07-09 01:07:40 +0300

    Update [id].js

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,8 +1,9 @@
 import {connectToDatabase} from "../../lib/mongodb";
 import fs from "fs";
 import {useRouter} from "next/router";
-import {Container, Grid, List, ListItem, ListItemButton, ListItemText, Paper} from "@mui/material";
-import {useState} from "react";
+import {Card, CardHeader, CardMedia, Container, Grid, Typography, CardContent, List} from "@mui/material";
+import Carousel from 'react-material-ui-carousel'
+import {ArrowLeft, ArrowRight} from "@mui/icons-material";
 
 
 export async function getStaticProps(ctx) {
@@ -11,13 +12,12 @@ export async function getStaticProps(ctx) {
     const {db} = await connectToDatabase();
     const cursor = await db.collection("Projects").find({_id: Number(id)}, {"hint": "all_fields"});
     const project = (await cursor.toArray())[0]
-    console.log(project)
     const images = [];
     for (let imageObject of project.images) {
-        const src = await fs.promises.readFile(imageObject.src, 'base64');
+        const image = await fs.promises.readFile(imageObject.src, 'base64');
         images.push({
             ...imageObject,
-            src: src
+            src: `data:${imageObject.type};base64,${image}`
         })
     }
 
@@ -47,83 +47,54 @@ export async function getStaticPaths() {
 
 const ProjectDetails = ({project}) => {
     const router = useRouter();
-    const [selected, setSelected] = useState(0)
 
-    // if (router.isFallback) {
-    //     return <Backdrop
-    //         sx={{bgcolor: 'white', zIndex: (theme) => theme.zIndex.drawer + 1}}
-    //         open={true}
-    //     >
-    //         <CircularProgress color="secondary" size={200}/>
-    //     </Backdrop>
-    // }
-    // const window = () => Window;
-    // const container = window !== undefined ? () => window().document?.body : undefined;
-    // const [mobileOpen, setMobileOpen] = useState(false);
-    // const handleDrawerToggle = () => {
-    //     setMobileOpen(!mobileOpen);
-    // };
-
-    const handleSelected = (id) => {
-        setSelected(id)
-    }
-
-    // const drawer = (
-    //     <List>
-    //         {['المعلومات الأساسية', 'معلومات البناء', 'معلومات المكتب'].map((text, index) => (
-    //             <ListItem key={text} disablePadding>
-    //                 <ListItemButton onClick={() => handleSelected(index)} selected={index === selected}>
-    //                     <ListItemText primary={text}/>
-    //                 </ListItemButton>
-    //             </ListItem>
-    //         ))}
-    //     </List>
-    // )
     return (
         <Container maxWidth={false} sx={{mb: 5, mt: 7, pl: {xs: 3, sm: 3, md: 3}}}>
             <Grid container>
-                {/*<Grid item xs={2} mr={2} component={Paper}>*/}
-                {/*    {*/}
-                {/*        drawer*/}
-                {/*    }*/}
-                {/*    <IconButton*/}
-                {/*        color="inherit"*/}
-                {/*        aria-label="open drawer"*/}
-                {/*        edge="start"*/}
-                {/*        onClick={handleDrawerToggle}*/}
-                {/*        sx={{mr: 2, display: {sm: 'none'}}}*/}
-                {/*    >*/}
-                {/*        <MenuIcon/>*/}
-                {/*    </IconButton>*/}
-                {/*    <Drawer*/}
-                {/*        container={container}*/}
-                {/*        variant="temporary"*/}
-                {/*        open={mobileOpen}*/}
-                {/*        onClose={handleDrawerToggle}*/}
-                {/*        ModalProps={{*/}
-                {/*            keepMounted: true, // Better open performance on mobile.*/}
-                {/*        }}*/}
-                {/*        sx={{*/}
-                {/*            display: {xs: 'block', sm: 'none'},*/}
-                {/*            '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},*/}
-                {/*        }}*/}
-                {/*    >*/}
-                {/*        {drawer}*/}
-                {/*    </Drawer>*/}
-                {/*    <Drawer*/}
-                {/*        variant="permanent"*/}
-                {/*        sx={{*/}
-                {/*            display: {xs: 'none', sm: 'block'},*/}
-                {/*            '& .MuiDrawer-paper': {boxSizing: 'border-box', width: drawerWidth},*/}
-                {/*        }}*/}
-                {/*        open*/}
-                {/*    >*/}
-                {/*        {drawer}*/}
-                {/*    </Drawer>*/}
-                {/*</Grid>*/}
-
-                <Grid item xs={} component={Paper}>
-                    {project?.projectName}
+                <Grid item xs={12}>
+                    <Card>
+                        <CardHeader
+                            title={
+                                <Grid container alignItems={'center'} justifyContent={'center'}>
+                                    <Typography variant={'h3'}>{project?.projectName}</Typography>
+                                </Grid>
+                            }
+                        />
+                        <Carousel
+                            sx={{marginTop: {xs: 5, lg: 10}, marginX: {xs: 0, lg: 13}, px: {xs: 0, lg: 10}}}
+                            NextIcon={<ArrowLeft sx={{fontSize: 40}}/>}
+                            PrevIcon={<ArrowRight sx={{fontSize: 40}}/>}
+                            width={'70rem'}
+                            height={'40rem'}
+                            // NavButtonsProps={{
+                            //     style: {
+                            //         color: 'black'
+                            //     }
+                            // }}
+                            // navButtonsWrapperProps={{
+                            //     style: {
+                            //         background: 'black',
+                            //         height: '50rem',
+                            //         opacity: 0.2
+                            //     }
+                            // }}
+                            navButtonsAlwaysVisible={true}
+                        >
+                            {project?.images?.map((image, i) => (
+                                <CardMedia
+                                    key={i}
+                                    component={'img'}
+                                    image={image.src}
+                                    width={'70rem'}
+                                    height={'100%'}
+                                    alt={image.name}
+                                />
+                            ))}
+                        </Carousel>
+                        <CardContent>
+                            <List></List>
+                        </CardContent>
+                    </Card>
                 </Grid>
             </Grid>
         </Container>
