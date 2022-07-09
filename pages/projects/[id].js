5445a088ac837ad68d8d3752047b4fba271ded82
Yousef500 <Kazuya14725@gmail.com>
2022-07-09 14:09:16 +0300

    almost donew with view

--- pages/projects/[id].js
+++ pages/projects/[id].js
@@ -1,71 +1,112 @@
-import {connectToDatabase} from "../../lib/mongodb";
+import { connectToDatabase } from "../../lib/mongodb";
 import fs from "fs";
-import {useRouter} from "next/router";
-import {Card, CardHeader, CardMedia, Container, Grid, Typography, CardContent, List} from "@mui/material";
-import Carousel from 'react-material-ui-carousel'
-import {ArrowLeft, ArrowRight} from "@mui/icons-material";
-
+import { useRouter } from "next/router";
+import {
+    Card,
+    CardHeader,
+    CardMedia,
+    Container,
+    Grid,
+    Typography,
+    CardContent,
+    List,
+    ListItem,
+    ListItemText,
+    Backdrop,
+    CircularProgress
+} from "@mui/material";
+import Carousel from "react-material-ui-carousel";
+import { ArrowLeft, ArrowRight } from "@mui/icons-material";
 
 export async function getStaticProps(ctx) {
-    const {id} = ctx.params;
+    const { id } = ctx.params;
 
-    const {db} = await connectToDatabase();
-    const cursor = await db.collection("Projects").find({_id: Number(id)}, {"hint": "all_fields"});
-    const project = (await cursor.toArray())[0]
+    const { db } = await connectToDatabase();
+    const cursor = await db
+        .collection("Projects")
+        .find({ _id: Number(id) }, { "hint": "all_fields" });
+    const project = (await cursor.toArray())[0];
     const images = [];
     for (let imageObject of project.images) {
-        const image = await fs.promises.readFile(imageObject.src, 'base64');
+        const image = await fs.promises.readFile(imageObject.src, "base64");
         images.push({
             ...imageObject,
-            src: `data:${imageObject.type};base64,${image}`
-        })
+            src: `data:${imageObject.type};base64,${image}`,
+        });
     }
 
     return {
         props: {
-            project: {...project, images: images}
-        }
-    }
+            project: { ...project, images: images },
+        },
+    };
 }
 
 export async function getStaticPaths() {
-    const {db} = await connectToDatabase()
-    const cursor = await db.collection("Projects").find({}, {"hint": "_id_"}).project({_id: 1});
+    const { db } = await connectToDatabase();
+    const cursor = await db
+        .collection("Projects")
+        .find({}, { "hint": "_id_" })
+        .project({ _id: 1 });
     const ids = await cursor.toArray();
     const paths = [];
     for (let projectId of ids) {
-        paths.push({params: {id: projectId.toString()}});
+        paths.push({ params: { id: projectId._id.toString() } });
     }
 
     return {
         paths,
-        fallback: true
-    }
+        fallback: true,
+    };
 }
 
 // const drawerWidth = 240;
 
-const ProjectDetails = ({project}) => {
+const ProjectDetails = ({ project }) => {
     const router = useRouter();
+    const {_id, images, mainImage, ...proj } = project ?? {};
+    console.log(project)
+
+    if (router.isFallBack || !project) {
+        return <Backdrop
+                            sx={{bgcolor: 'white', zIndex: (theme) => theme.zIndex.drawer + 1}}
+                            open={true}
+                        >
+                            <CircularProgress color="secondary" size={200}/>
+                        </Backdrop>
+    }
 
     return (
-        <Container maxWidth={false} sx={{mb: 5, mt: 7, pl: {xs: 3, sm: 3, md: 3}}}>
+        <Container
+            maxWidth={false}
+            sx={{ mb: 5, mt: 7, pl: { xs: 3, sm: 3, md: 3 } }}
+        >
             <Grid container>
                 <Grid item xs={12}>
                     <Card>
                         <CardHeader
                             title={
-                                <Grid container alignItems={'center'} justifyContent={'center'}>
-                                    <Typography variant={'h3'}>{project?.projectName}</Typography>
+                                <Grid
+                                    container
+                                    alignItems={"center"}
+                                    justifyContent={"center"}
+                                >
+                                    <Typography variant={"h3"}>
+                                        {project?.projectName}
+                                    </Typography>
                                 </Grid>
                             }
                         />
                         <Carousel
-                            sx={{marginTop: {xs: 5, lg: 10}, marginX: {xs: 0, lg: 13}, px: {xs: 0, lg: 10}}}
-                            NextIcon={<ArrowLeft sx={{fontSize: 40}}/>}
-                            PrevIcon={<ArrowRight sx={{fontSize: 40}}/>}
-                            width={'70rem'}
-                            height={'40rem'}
+                            sx={{
+                                marginTop: { xs: 5, lg: 10 },
+                                marginX: { xs: 0, lg: 13 },
+                                px: { xs: 0, lg: 10 },
+                            }}
+                            NextIcon={<ArrowLeft sx={{ fontSize: 40 }} />}
+                            PrevIcon={<ArrowRight sx={{ fontSize: 40 }} />}
+                            width={"70rem"}
+                            height={"40rem"}
                             // NavButtonsProps={{
                             //     style: {
                             //         color: 'black'
@@ -83,22 +124,29 @@ const ProjectDetails = ({project}) => {
                             {project?.images?.map((image, i) => (
                                 <CardMedia
                                     key={i}
-                                    component={'img'}
+                                    component={"img"}
                                     image={image.src}
-                                    width={'70rem'}
-                                    height={'100%'}
+                                    width={"70rem"}
+                                    height={"100%"}
                                     alt={image.name}
                                 />
                             ))}
                         </Carousel>
                         <CardContent>
-                            <List></List>
+                            <List>
+                                {proj &&
+                                    Object.values(proj).map((entry, ind) => (
+                                        <ListItem key={ind}>
+                                            <ListItemText>{entry}</ListItemText>
+                                        </ListItem>
+                                    ))}
+                            </List>
                         </CardContent>
                     </Card>
                 </Grid>
             </Grid>
         </Container>
-    )
-}
+    );
+};
 
-export default ProjectDetails
+export default ProjectDetails;
